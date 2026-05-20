21e2e2585485067c53baa88d099cee5b54cd0bef
sashatop228 <qwetrtrtyuip@gmail.com>
2026-05-20 15:31:42 +0300

    Add files via upload

---
+++ config/config.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'host'     => 'имя.up.railway.app',
+    'port'     => 5432,
+    'dbname'   => 'railway',
+    'user'     => 'postgres',
+    'password' => 'ваш_пароль',
+    'sslmode'  => 'require',
+];
